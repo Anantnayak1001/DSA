3ce07e7465c9a61ff27ec86ad8cace267c08a5d7
Anantnayak1001 <Anantnayak121@gmail.com>
2025-08-13 22:55:55 +0530

    Create Question4(a).cpp

---
+++ Assignment 1/Question4(a).cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+void swapnum(int &a, int &b){
+    int temp= a;
+    a=b;
+    b=temp;
+}
+void Display(int* arr, int n){
+	for(int i=0; i<n; i++){
+		cout<<arr[i]<<"\t";
+	}
+}
+
+int main()
+{
+  int arr[6]={10,20,30,40,50,60};
+   
+   int size=6;
+   
+   for(int i=0; i<size/2; i++){
+       swap(arr[i], arr[size-i-1]);
+   }
+    Display(arr,size);
+  return 0;
+}
